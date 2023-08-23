21e7de67fe2e9613cda0637900e75e5b5607e38b
Allan Scala <allanscala@gmail.com>
2023-08-23 13:36:05 -0300

    update base url

--- src/test/java/Utils/Configuration.java
+++ src/test/java/Utils/Configuration.java
@@ -1,7 +1,7 @@
 package Utils;
 
 public class Configuration {
-    private final String baseUrl = "http://localhost:3000";
+    private final String baseUrl = "http://127.0.0.1:3000";
     public String getBaseUrl() {
         return baseUrl;
     }
